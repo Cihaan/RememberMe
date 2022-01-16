6e49cec12a3c2481f801bdd5e445e3065e6e8328
Cihaan <guide7169@gmail.com>
2022-01-16 16:04:03 +0100

    npm i + initialisation App.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedRessources: [
+        {
+          id: 'official-guide',
+          tittle: 'Official Guide',
+          description: 'The official VueJS documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          tittle: 'Google',
+          description: 'Find everything.',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
